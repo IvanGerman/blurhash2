3791165b6c57e1a182097bd8d5bfb651ad14a4a0
IvanGerman <ivgvi@mail.ru>
2023-03-13 18:16:13 +0700

    init: start project blurhash2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import {Blurhash} from 'react-blurhash';
+import {motion} from 'framer-motion';
+import {useEffect, useState} from 'react';
+import {encodeImageToBlurhash} from './encoder';
+import './style.css';
 
-function App() {
+export default function App() {
+  const [hashUrl, setHashUrl] = useState('U27UC_Tg00D$.AW?E1nN00EA%4~7ISMw%2-p');
+  const [imageLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getHashUrl() {
+      console.log('getHashUrl');
+      const hash = await encodeImageToBlurhash('https://images.unsplash.com/photo-1617854818583-09e7f077a156?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format');
+      // const hash = await encodeImageToBlurhash(mountain);
+      setHashUrl(hash);
+    }
+    getHashUrl();
+  }, []);
+  
+
+  const onLoaded = () => {
+    setLoading(false);
+  };
+
+  console.log('hashUrl', hashUrl);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="blurhash-wrapper">
+      <Blurhash hash={hashUrl} width={300} height={200} />
+      <motion.img
+        initial={{opacity: 0}}
+        // style={{ height: imageLoading ? "6rem" : "auto" }}
+        animate={{
+          opacity: imageLoading ? 0 : 1,
+        }}
+        transition={{opacity: {delay: 0.5, duration: 0.4}}}
+        loading="lazy"
+        onLoad={onLoaded}
+        className="image"
+        src="https://images.unsplash.com/photo-1617854818583-09e7f077a156?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format"
+        
+        // src={mountain}
+        width={300}
+        height={200}
+      />
     </div>
   );
-}
 
-export default App;
+}
